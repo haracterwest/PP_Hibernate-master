4938adaf47a0dbe2e16dba7f67988a3c2f87ac49
haracterwest <haracter.west@gmail.com>
2023-06-02 16:52:46 +0300

    final commit

--- PP_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        Util.getConnection();
+        UserServiceImpl userDaoJDBC = new UserServiceImpl();
+        userDaoJDBC.dropUsersTable();
+        userDaoJDBC.createUsersTable();
+
+        userDaoJDBC.saveUser("Name1", "LastName1", (byte) 20);
+        userDaoJDBC.saveUser("Name2", "LastName2", (byte) 25);
+        userDaoJDBC.saveUser("Name3", "LastName3", (byte) 31);
+        userDaoJDBC.saveUser("Name4", "LastName4", (byte) 38);
+
+        List<User> allUsers = userDaoJDBC.getAllUsers();
+        System.out.println("ВСЕ: " + allUsers.get(0).getName() + allUsers.get(1).getName() + allUsers.get(2).getName() + allUsers.get(3).getName());
+        userDaoJDBC.removeUserById(1);
+        userDaoJDBC.cleanUsersTable();
     }
 }
--- PP_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String URL = "jdbc:postgresql://localhost:5432/db";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "postgres";
+
+    public static Connection getConnection() {
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Соединение с БД - OK!");
+        } catch (SQLException e) {
+            System.out.println("Соединение с БД - ERROR");
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
